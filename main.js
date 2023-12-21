9d43ef0574890e29d47f53c591630e5ad3cd076e
franklin-raph <igboekwulusifranklin@gmail.com>
2023-12-21 17:10:35 +0100

    added the faq section

--- main.js
+++ main.js
@@ -19,3 +19,30 @@ var swiper = new Swiper(".mySwiper", {
 $(document).ready(function() {
 	$('.carousel').carousel();
 });
+
+
+
+// FAQ STARTS
+
+	const faqItems = document.querySelectorAll('.faq-item');
+
+	faqItems.forEach(item => {
+		const question = item.querySelector('.faq-question');
+		
+		question.addEventListener('click', () => {
+			console.log(question);
+			// faqItems.forEach(otherItem => {
+			// 	if (otherItem !== item && otherItem.classList.contains('open')) {
+			// 		otherItem.classList.remove('open');
+			// 		otherItem.querySelector('.faq-answer').style.maxHeight = '0';
+			// 	}
+			// });
+
+			item.classList.toggle('open');
+			const answer = item.querySelector('.faq-answer');
+			answer.style.maxHeight = item.classList.contains('open') ? answer.scrollHeight + 'px' : '0';
+			console.log(answer.scrollHeight);
+		});
+	});
+
+// FAQ ENDS
