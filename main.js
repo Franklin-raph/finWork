937d4c092c8786a5326a2f81497671d5f0fcb5dd
franklin-raph <igboekwulusifranklin@gmail.com>
2023-12-21 14:34:44 +0100

    update

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+	grabCursor: true,
+	effect: "creative",
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+	creativeEffect: {
+	  prev: {
+		shadow: true,
+		translate: [0, 0, -400],
+	  },
+	  next: {
+		translate: ["100%", 0, 0],
+	  },
+	},
+  });
+
 $(document).ready(function() {
 	$('.carousel').carousel();
 });
